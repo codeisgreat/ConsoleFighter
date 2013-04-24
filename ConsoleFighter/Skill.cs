06973b8306aeb463f395952875721aafbd5c15a2
NekuSoul <Github@NekuSoul.de.vu>
2013-04-24 21:59:28 +0200

    Added 3 new Spells

--- ConsoleFighter/Skill.cs
+++ ConsoleFighter/Skill.cs
@@ -33,4 +33,51 @@
             return "Wait";
         }
     }
+
+    public class Weaken : Skill
+    {
+        public void ExecuteSkill(Hero Attacker, Hero Defender)
+        {
+            if (Defender.Attack > 5)
+            {
+                Defender.Attack -= 5;
+            }
+
+        }
+
+
+        public string GetName()
+        {
+            return "Weaken";
+        }
+    }
+
+    public class Fire : Skill
+    {
+        public void ExecuteSkill(Hero Attacker, Hero Defender)
+        {
+            Defender.Health -= 5 + Attacker.Magic - Defender.Magic;
+        }
+
+
+        public string GetName()
+        {
+            return "Fire";
+        }
+    }
+
+    public class Shield : Skill
+    {
+        public void ExecuteSkill(Hero Attacker, Hero Defender)
+        {
+            Attacker.Attack_Defense += 5;
+            Attacker.Magic_Defense += 5;
+        }
+
+
+        public string GetName()
+        {
+            return "Shield";
+        }
+    }
 }
